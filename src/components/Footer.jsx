45e99a1777d0c2bbe6f2cf7f63d11f04bd3e0e9f
Manvendra2509 <manvendra.17cse468@rtu.ac.in>
2023-06-28 18:12:27 +0530

    Added react components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  console.log(currentYear);
+
+  return (
+    <footer>
+      <p>Copyright ⓒ {currentYear}</p>
+    </footer>
+  );
+}
+
+export default Footer;
